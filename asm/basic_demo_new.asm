0d1b7b281317a45afe6187e246a7e59aba4546c3
SirMattyBoi <84469012+SirMattyBoi@users.noreply.github.com>
2023-04-18 13:27:28 -0400

    Update basic_demo_new.asm

--- asm/basic_demo_new.asm
+++ asm/basic_demo_new.asm
@@ -1,38 +1,39 @@
-; basic_demo_new.asm
-; updates output when sw9 is up
+; basicdemo.asm
+; Polls peripheral and lights an LED if it returns TRUE
 ; Assumes that peripheral has device ID 0x50 (&H50)
 ;
 ; ECE 2031 L08
 
-LOAD Zero
-OUT LEDs
-
 Start:
-    IN      SWITCHES      ;takes in data from switches
-    AND     POLL          ;sees if sw9 is up
-    JZERO   START
-
-;--turns on the LED lights--
-    IN Peripheral            ;loads full value into AC
-    STORE pDATA
-    AND One
-    LOAD Full
-    OUT LEDs
-    LOAD pDATA
-    SHIFT -1
-    OUT Hex0
-    JUMP    Start           ;jumps back to beginning
+	IN Switches
+	AND SW9 ; checks to see if switch 9 is up
+	JPOS Reset
+	;if not reset, display the snap and counter on hex1 and hex0
+	IN Peripheral_Snap
+	OUT Hex1
+	IN Peripheral_Counter
+	OUT Hex0
+	JUMP Start
+	
+	
+Reset:
+	LOAD Zero
+	OUT Peripheral_Reset
+	OUT Hex1 
+	OUT Hex0
+	JUMP Start
 
 
 ; Constants
 Zero:   DW 0
-One: DW 1
 Full:   DW &HFFFF
+SW9: DW &H0200
+
 Switches:  EQU 000
 LEDs:      EQU 001
 Timer:     EQU 002
 Hex0:      EQU 004
 Hex1:      EQU 005
-Peripheral: EQU &H050
-POLL:  DW &H0200
-pData: DW 0
+Peripheral_Snap: EQU &H050
+Peripheral_Counter: EQU &H051
+Peripheral_Reset: 	EQU &H052
